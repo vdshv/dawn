7f2a86ec0f4203e109df1f1aca9b28d007d0f5c1
Vadym Shevchenko <vdshvch@gmail.com>
2023-05-21 15:53:00 +0300

    fonts

--- assets/global.js
+++ assets/global.js
@@ -1,3 +1,41 @@
+// constants.js
+const ON_CHANGE_DEBOUNCE_TIMER = 300;
+
+const PUB_SUB_EVENTS = {
+  cartUpdate: 'cart-update',
+  quantityUpdate: 'quantity-update',
+  variantChange: 'variant-change',
+  cartError: 'cart-error'
+};
+// end constants.js
+
+// pubsub.js
+let subscribers = {}
+
+function subscribe(eventName, callback) {
+  if (subscribers[eventName] === undefined) {
+    subscribers[eventName] = []
+  }
+
+  subscribers[eventName] = [...subscribers[eventName], callback];
+
+  return function unsubscribe() {
+    subscribers[eventName] = subscribers[eventName].filter((cb) => {
+      return cb !== callback
+    });
+  }
+};
+
+function publish(eventName, data) {
+  if (subscribers[eventName]) {
+    subscribers[eventName].forEach((callback) => {
+      callback(data)
+    })
+  }
+}
+// end pubsub.js
+
+
 class SliderComponent extends HTMLElement {
   constructor() {
       super();
